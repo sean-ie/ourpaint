cd5c1f47d041b23d96bc3f317969fab4d46ff3af
Lorraine Chambers <90639741+Lorraine-C@users.noreply.github.com>
2022-02-19 12:34:34 +0000

    Update paint.rb

--- app/models/paint.rb
+++ app/models/paint.rb
@@ -3,6 +3,6 @@ class Paint < ApplicationRecord
   validates :brand, presence: true
   validates :colour, presence: true
   validates :volume, presence: true, numericality: true
-  validated :coordinates, presence: true, numericality: true
+  validates :coordinates, presence: true, numericality: true
   has_one_attached :photo
 end
