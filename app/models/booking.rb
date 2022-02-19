225b810ba70f47bd6552c997b4136fb8b08de4be
Matthew Gibb <87804850+matthewg543@users.noreply.github.com>
2022-02-19 12:27:42 +0000

    created routes and updated models with assosciations and validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :paint
+  validates :user, presence: true
+  validates :paint, presence: true
+  validtes :datetime, presence: true
 end
